efe87c9b32ab0a91becea8f3ae874d6074c957c1
Arhyel24 <arhyelphilip024@gmail.com>
2024-09-19 19:03:26 +0100

    Minor text alignment fix login screen

--- app/components/login.tsx
+++ app/components/login.tsx
@@ -112,7 +112,7 @@ const LoginPage = () => {
           )}
           <button
             type="submit"
-            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 flex items-center text-center"
+            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 flex items-center text-center justify-center"
           >
             {loading ? (
               <>
